ce21bdbcda318fdb4136a83d427655d3d987eddb
Do Dinh Trung (RBVH/ETM5) <drd1hc@bosch.com>
2019-05-06 10:57:58 +0700

    Backend server running (need to implement optional server selection in argparse)

--- src/main.py
+++ src/main.py
@@ -12,8 +12,6 @@ from web_scrapper.controller.scrapper_controller import scrapper_controller
 from pattern_matcher.controller.pattern_matcher_controller import pattern_matcher_controller
 from web_scrapper.websocket.ws_server import *
 
-from web_scrapper.sentiment_analyzer.analyzer import headlineAnalyzer
-
 app = Flask(__name__)
 
 # Setup logging config
@@ -21,53 +19,39 @@ setup_logging()
 CONFIG = get_scrapper_config()
 LOGGER = logging.getLogger(__name__)
 
-# def start_server(is_logging):
-# 	start_websocket_server(is_logging)
-# 	backend_api_thread = threading.Thread(target=start_api_server)
-# 	backend_api_thread.start()
+def start_server(is_logging):
+	start_websocket_server(is_logging)
+	backend_api_thread = threading.Thread(target=start_api_server)
+	backend_api_thread.start()
 
-# def start_api_server():
-# 	app.register_blueprint(scrapper_controller, url_prefix='/api/v0/scraper')
-# 	app.register_blueprint(pattern_matcher_controller, url_prefix='/api/v0/matcher')
-# 	app.run(host=CONFIG['SERVER']['HOST'], 
-# 			port=CONFIG['SERVER']['PORT'], 
-# 			threaded=True,
-# 			debug=False)
+def start_api_server():
+	app.register_blueprint(scrapper_controller, url_prefix='/api/v0/scraper')
+	app.register_blueprint(pattern_matcher_controller, url_prefix='/api/v0/matcher')
+	app.run(host=CONFIG['SERVER']['HOST'], 
+			port=CONFIG['SERVER']['PORT'], 
+			threaded=True,
+			debug=False)
 
-# def start_websocket_server(is_logging):
-# 	ws_ip = CONFIG['WEBSOCKET_SERVER']['HOST']
-# 	ws_route = CONFIG['WEBSOCKET_SERVER']['ROUTE']
-# 	ws_port = CONFIG['WEBSOCKET_SERVER']['PORT']
+def start_websocket_server(is_logging):
+	ws_ip = CONFIG['WEBSOCKET_SERVER']['HOST']
+	ws_route = CONFIG['WEBSOCKET_SERVER']['ROUTE']
+	ws_port = CONFIG['WEBSOCKET_SERVER']['PORT']
 
-# 	application = WSServer([(ws_route, WSHandler)])
-# 	application.listen(ws_port)
+	application = WSServer([(ws_route, WSHandler)])
+	application.listen(ws_port)
 
-# 	LOGGER.info('Websocket Server Started at ws://{}:{}{}'.format(ws_ip, ws_port, ws_route))
-# 	scrapper_thread = threading.Thread(target=application.scrape, args=[is_logging])
-# 	tornado_thread = threading.Thread(target=tornado.ioloop.IOLoop.instance().start)
+	LOGGER.info('Websocket Server Started at ws://{}:{}{}'.format(ws_ip, ws_port, ws_route))
+	scrapper_thread = threading.Thread(target=application.scrape, args=[is_logging])
+	tornado_thread = threading.Thread(target=tornado.ioloop.IOLoop.instance().start)
 	
-# 	scrapper_thread.start()
-# 	tornado_thread.start()
-
-# if __name__ == "__main__":
-# 	parser = argparse.ArgumentParser(description='Stock news webscrapper & sentiment analysis')
-# 	parser.add_argument('--logging', metavar='LOGGING', default=0, type=int, help='Log scrapping outputs into logs/*.json')
-# 	args = parser.parse_args()
-
-# 	# Start service
-# 	is_logging = bool(args.logging)
-# 	start_server(is_logging)
+	scrapper_thread.start()
+	tornado_thread.start()
 
 if __name__ == "__main__":
-	a = headlineAnalyzer("Apple is acquired by Amazon", "apple", True)
-	a.analyze()
-	a = headlineAnalyzer("Amazon acquires Apple", "apple", True)
-	a.analyze()
-	a = headlineAnalyzer("Apple to be acquired by Amazon", "apple", True)
-	a.analyze()
-	a = headlineAnalyzer("Amazon to be acquired by Apple", "apple", True)
-	a.analyze()
-	a = headlineAnalyzer("Apple is acquiring Amazon", "apple", True)
-	a.analyze()
-	a = headlineAnalyzer("Apple acquired by Amazon", "apple", True)
-	a.analyze()
+	parser = argparse.ArgumentParser(description='Stock news webscrapper & sentiment analysis')
+	parser.add_argument('--logging', metavar='LOGGING', default=0, type=int, help='Log scrapping outputs into logs/*.json')
+	args = parser.parse_args()
+
+	# Start service
+	is_logging = bool(args.logging)
+	start_server(is_logging)
