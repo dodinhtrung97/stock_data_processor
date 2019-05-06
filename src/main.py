7ff21c90cd9ab25121f91e9bd63ad620ffa50c64
Do Dinh Trung (RBVH/ETM5) <drd1hc@bosch.com>
2019-05-06 11:55:06 +0700

    Updated README

--- src/main.py
+++ src/main.py
@@ -19,14 +19,19 @@ setup_logging()
 CONFIG = get_scrapper_config()
 LOGGER = logging.getLogger(__name__)
 
-def start_server(is_logging):
-	start_websocket_server(is_logging)
-	backend_api_thread = threading.Thread(target=start_api_server)
+def start_server(args):
+	if bool(args.websocket):
+		start_websocket_server(bool(args.logging))
+
+	backend_api_thread = threading.Thread(target=start_api_server, args=[args,])
 	backend_api_thread.start()
 
-def start_api_server():
-	app.register_blueprint(scrapper_controller, url_prefix='/api/v0/scraper')
-	app.register_blueprint(pattern_matcher_controller, url_prefix='/api/v0/matcher')
+def start_api_server(args):
+	if bool(args.scraper):
+		app.register_blueprint(scrapper_controller, url_prefix='/api/v0/scraper')
+	if bool(args.matcher):
+		app.register_blueprint(pattern_matcher_controller, url_prefix='/api/v0/matcher')
+	
 	app.run(host=CONFIG['SERVER']['HOST'], 
 			port=CONFIG['SERVER']['PORT'], 
 			threaded=True,
@@ -49,9 +54,11 @@ def start_websocket_server(is_logging):
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Stock news webscrapper & sentiment analysis')
+	parser.add_argument('--websocket', metavar='WEBSOCKET', default=1, type=int, help='Determine if websocket for auto_scraper module will be started')
+	parser.add_argument('--scraper', metavar='SCRAPPER', default=1, type=int, help='Determine if scraper module will be started')
+	parser.add_argument('--matcher', metavar='MATCHER', default=1, type=int, help='Determine if pattern matcher module will be started')
 	parser.add_argument('--logging', metavar='LOGGING', default=0, type=int, help='Log scrapping outputs into logs/*.json')
 	args = parser.parse_args()
 
 	# Start service
-	is_logging = bool(args.logging)
-	start_server(is_logging)
+	start_server(args)
