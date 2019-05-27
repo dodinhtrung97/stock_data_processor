fbd4e03b732c460e11cf1a03883d780e0ebc324a
Do Dinh Trung (RBVH/ETM5) <drd1hc@bosch.com>
2019-05-27 17:26:24 +0700

    Updated requirements.txt

--- service_start.bat
+++ service_start.bat
@@ -6,5 +6,8 @@ set SERVICE_NAME=Scheduled Data Collector
 
 set SERVICE_LOG_PATH=%PWD%\%DEFAULT_SERVICE_LOG_DIRECTORY_NAME%
 
+echo INFO: Installing pip dependencies
+start cmd /k pip install -r requirements.txt
+
 echo INFO: Starting %SERVICE_NAME% service with logs at %SERVICE_LOG_PATH%
 start cmd /k python src\data_collector_service.py debug --log_dir=%SERVICE_LOG_PATH%
