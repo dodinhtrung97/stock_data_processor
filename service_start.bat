d445e29697e983f1ccf1b35c292126a088b349f7
Do Dinh Trung (RBVH/ETM5) <drd1hc@bosch.com>
2019-05-27 17:14:00 +0700

    Finished start script

---
+++ service_start.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set PWD=%cd%
+set DEFAULT_SERVICE_LOG_DIRECTORY_NAME=logs
+set SERVICE_NAME=Scheduled Data Collector
+
+set SERVICE_LOG_PATH=%PWD%\%DEFAULT_SERVICE_LOG_DIRECTORY_NAME%
+
+echo INFO: Starting %SERVICE_NAME% service with logs at %SERVICE_LOG_PATH%
+start cmd /k python src\data_collector_service.py debug --log_dir=%SERVICE_LOG_PATH%
