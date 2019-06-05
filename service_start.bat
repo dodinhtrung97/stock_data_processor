9b9ac6668de8ddcf2084994616c144ab3dda7e30
Do Dinh Trung (RBVH/ETM5) <Trung.DoDinh@vn.bosch.com>
2019-06-05 09:38:10 +0700

    Fixed bugs in service batch script where current directory could not be located in admin mode

--- service_start.bat
+++ service_start.bat
@@ -1,20 +1,22 @@
 @echo off
-cd %0\..
 
-set PWD=%cd%
+set PWD=%~dp0
+echo %PWD%
 set DEFAULT_SERVICE_LOG_DIRECTORY_NAME=logs
 set SERVICE_NAME=Scheduled Data Collector
 
 set SERVICE_LOG_PATH=%PWD%\%DEFAULT_SERVICE_LOG_DIRECTORY_NAME%
+set SERVICE_RUNNER_PATH=%PWD%\src\data_collector_service.py
+set SERVICE_REQUIREMENT_PATH=%PWD%\requirements.txt
 
 echo INFO: Using default .\logs directory for service logging
 if not exist %SERVICE_LOG_PATH% call md %SERVICE_LOG_PATH%
 
 echo INFO: Installing pip dependencies
-call pip install -r requirements.txt
+call pip install -r %SERVICE_REQUIREMENT_PATH%
 
 echo INFO: Starting %SERVICE_NAME% service with logs at %SERVICE_LOG_PATH%
-call python src\data_collector_service.py install
+call python %SERVICE_RUNNER_PATH% install
 call python src\data_collector_service.py start --log_dir=%SERVICE_LOG_PATH%
 
 echo INFO: Service %SERVICE_NAME% started
